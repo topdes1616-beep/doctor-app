5dd16389ef3ce2907499ee01117c431062058847
topdes1616-beep <topdes1616@gmail.com>
2026-06-02 21:29:02 +0100

    Update MainActivity.kt

--- app/src/main/kotlin/com/clinic/radar/MainActivity.kt
+++ app/src/main/kotlin/com/clinic/radar/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.clinic.radar
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.webkit.JsResult
 import android.webkit.JsPromptResult
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -15,9 +19,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
     private var localServer: LocalWebServer? = null
+    private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
 
     companion object {
         private const val LOCAL_PORT = 8787
+        private const val FILE_CHOOSER_REQUEST = 1001
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +43,23 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
 
         webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowFileChooser(
+                view: WebView,
+                callback: ValueCallback<Array<Uri>>,
+                params: FileChooserParams
+            ): Boolean {
+                fileUploadCallback?.onReceiveValue(null)
+                fileUploadCallback = callback
+                val intent = params.createIntent()
+                try {
+                    startActivityForResult(intent, FILE_CHOOSER_REQUEST)
+                } catch (e: Exception) {
+                    fileUploadCallback = null
+                    return false
+                }
+                return true
+            }
 
             override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
                 AlertDialog.Builder(this@MainActivity)
@@ -72,6 +95,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.loadUrl("http://localhost:$LOCAL_PORT/")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+            val results = if (resultCode == Activity.RESULT_OK) {
+                WebChromeClient.FileChooserParams.parseResult(resultCode, data)
+            } else {
+                null
+            }
+            fileUploadCallback?.onReceiveValue(results)
+            fileUploadCallback = null
+        }
     }
 
     override fun onBackPressed() {
